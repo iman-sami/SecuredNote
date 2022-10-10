d2b6aa9e47ecdb6a2c840afe2bf9caeda3ec7668
iman-sami <imansamifaith@gmail.com>
2022-10-10 21:51:21 +0300

    bottom nav set up with navcontroller

--- app/src/main/java/com/imanp/securenote/MainActivity.kt
+++ app/src/main/java/com/imanp/securenote/MainActivity.kt
@@ -2,10 +2,30 @@ package com.imanp.securenote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.imanp.securenote.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = findNavController(R.id.main_fragment)
+        binding.bottomNavigation.setupWithNavController(navController)
+        binding.bottomNavigation.setOnItemSelectedListener { item->
+            when(item.itemId){
+                R.id.home-> navController.navigate(R.id.homeFragment)
+                R.id.fav->navController.navigate(R.id.likedFragment)
+                R.id.setting->navController.navigate(R.id.settingFragment)
+            }
+            true
+        }
+
     }
 }
